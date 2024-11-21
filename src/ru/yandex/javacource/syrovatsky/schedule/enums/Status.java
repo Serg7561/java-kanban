741f0e1faa7b6914e947221cbe92817cd0d08e6e
Serg7561 <ss13.sar@yandex.ru>
2024-11-21 22:01:02 +0300

    Пятый спринт устранение замечаний V 3

--- src/ru/yandex/javacource/syrovatsky/schedule/enums/Status.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.javacource.syrovatsky.schedule.enums;
-
-public enum Status {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
-
--- src/ru/yandex/javacource/syrovatsky/schedule/task/Status.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.javacource.syrovatsky.schedule.task;
-
-public enum Status {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
-
